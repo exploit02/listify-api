834abc6ee67f140caefedfa83935b3ce62b79cb4
Srimanta Ghosh <srimanta@gmail.com>
2020-06-29 02:58:09 +0530

    Status Wise APIs

--- routes/v2/statusWise.js
+++ routes/v2/statusWise.js
@@ -1,7 +1,29 @@
 var statusWise = require('express').Router()
+var ObjectId = require("mongodb").ObjectID;
 
 statusWise.get('/', (req, res, next)=>{
-    
+    req.db.collection('lists').aggregate([
+        {
+            $match: {userid: ObjectId(req.jwtpayload.data._id)}
+        },
+        {
+          $group : { _id : "$completed", lists: { $push: "$$ROOT" }, count: {$sum: 1} }
+        }
+        
+      ]).toArray().then(result=>{
+          res.status(200).json({
+              success: true,
+              message: "Listitems fetched successfully",
+              result: result
+          })
+      })
+      .catch(err=>{
+          res.status(500).json({
+              success: true,
+              message: "Internal server error !!!",
+              result: null
+          })
+      })
 })
 
 module.exports = statusWise;
